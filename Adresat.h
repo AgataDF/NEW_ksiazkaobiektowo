3fbbb1bb85fab8280d8a10fb1ccda8a0e7aa7ed4
Agata Dziuba-Flizikowska <agata.dziuba.programista@gmail.com>
2022-06-28 21:35:23 +0200

    Added struct Addressee

--- Adresat.h
+++ Adresat.h
@@ -7,6 +7,24 @@ using namespace std;
 
 class Adresat
 {
+    int id, idUzytkownika;
+    string imie, nazwisko, numerTelefonu, email, adres;
 
+public:
+    void ustawId (int noweId);
+    void ustawIdUzytkownika (int noweId);
+    void ustawImie (string noweImie);
+    void ustawNazwisko (string noweNazwisko);
+    void ustawNumerTelefonu (string nowyNumer);
+    void ustawEmail (string nowyEmail);
+    void ustawAdres (string nowyAdres);
+
+    int pobierzId();
+    int pobierzIdUzytkownika();
+    string pobierzImie();
+    string pobierzNazwisko();
+    string pobierzNumerTelefonu();
+    string pobierzEmail();
+    string pobierzAdres();
 };
 #endif
